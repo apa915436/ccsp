80783c58a0906206378da774f92aa2437db0f82a
Andy Chang <andyaoao@gmail.com>
2014-05-26 14:46:42 +0800

    layout changed

--- public/js/home.js
+++ public/js/home.js
@@ -1,8 +1,17 @@
 (function(){
 
-var addButton = $('#add'),
-	chartity_btn = $('#charity'),
-    trade_btn = $('#trade');
+var addButton       = $('#add'),
+	chartity_btn    = $('#charity'),
+    trade_btn       = $('#trade'),
+    latest_btn      = $('#latest'),
+    cloth_btn       = $('#cloth'),
+    house_btn家電   = $('#house'),
+    stationary_btn  = $('#stationary'),
+    food_btn        = $('#food'),
+    furniture_btn   = $('#furniture'),
+    3C_btn          = $('#3C'),
+    kitchen_btn     = $('#kitchen'),
+    clean_btn       = $('#clean'),
 
 var item = '<div class="col-sm-4"><div class="thumbnail"><span class="holder"><img src="../img/mouse.png" alt="" width="150px" height="150px"></span><div class="caption"><h3></h3><p></p><p><a href="#" class="btn btn-info">More</a></p></div></div></div>';
 var flag = $('.modify');
@@ -16,9 +25,7 @@ function load(){
     {},
     function(latests){
         $(flag).empty();
-        // console.log('callback');
         $(item).appendTo(flag).find('h3').text('latest item');
-        // $('this').parent('li').addClass('active');
         }
     // ,'json'
     );
@@ -36,8 +43,6 @@ chartity_btn.click(function(){
         var a= $(chartity_btn).parent('li');
         a.addClass('active');
         a.prev().removeClass('active');
-        console.log(chartity_btn);
-        console.log('charity');
     }
     // ,'json'
   );
@@ -55,13 +60,29 @@ trade_btn.click(function(){
         var a= $(trade_btn).parent('li');
         a.addClass('active');
         a.next().removeClass('active');
+    }
+    // ,'json'
+  );
+});
+
+latest_btn.click(function(){
+    console.log('charity button');
+    
+    $.get(
+        '/latest',
+    {},
+    function(){
+        $(flag).empty();
+        $(item).appendTo(flag).find('h3').text('trade');
+        var a= $(trade_btn).parent('li');
+        a.addClass('active');
+        a.next().removeClass('active');
         console.log(chartity_btn);
         console.log('charity');
     }
     // ,'json'
   );
 });
-
 // $('#myModal').modal(options);
 
 
