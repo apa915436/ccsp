ca365864a0158cb77071b120b66e30ac3a058dd4
shelley57123 <shelleysun1992@gmail.com>
2014-05-29 00:43:26 +0800

    register

--- routes/index.js
+++ routes/index.js
@@ -1,103 +1,117 @@
-var mongoose = require( 'mongoose' );
+var mongoose = require('mongoose');
 // var promise = require( 'promise');
-var Supply   = mongoose.model('Supply');
-var Need   = mongoose.model('Need');
+var Supply = mongoose.model('Supply');
+var Need = mongoose.model('Need');
 
 
-exports.index = function (req, res) {
-  	res.render('index', {title: 'Love Spreading'});
+exports.index = function(req, res) {
+	res.render('index', {
+		title: 'Love Spreading'
+	});
 }
 
-exports.home = function (req, res) {
-  	res.render('home', {title: 'Love Spreading'});
+exports.home = function(req, res) {
+	console.log(req.session.user);
+	res.render('home', {
+		title: 'Love Spreading',
+		user: req.session.user
+	});
 }
 
-exports.supply = function (req, res) {
- 	// console.log('in latest model');
- 	console.log('in server ',req.params.id);
- 	Supply.
- 		find({name: req.params.id}).
- 		exec( function (err, items){
-	    	if(err){
-	      		console.error(err);
-	    	};
-	    	res.json(items);
-	    });       		 	
+exports.supply = function(req, res) {
+	// console.log('in latest model');
+	console.log('in server ', req.params.id);
+	Supply.
+	find({
+		name: req.params.id
+	}).
+	exec(function(err, items) {
+		if (err) {
+			console.error(err);
+		};
+		res.json(items);
+	});
 }
 
-exports.need = function (req, res) {
- 	console.log('in need model');
- 	console.log('in server ',req.params.id);
- 	Need.
- 		find({name: req.params.id}).
- 		exec( function (err, items){
-	    	if(err){
-	      		console.error(err);
-	    	};
-	    	res.json(items);
-	    });       		 	
+exports.need = function(req, res) {
+	console.log('in need model');
+	console.log('in server ', req.params.id);
+	Need.
+	find({
+		name: req.params.id
+	}).
+	exec(function(err, items) {
+		if (err) {
+			console.error(err);
+		};
+		res.json(items);
+	});
 }
 
-exports.create = function ( req, res){
-  	// console.log(req.body.name);
-  	new Need({
-      	name   : req.body.name,
-      	updated_at : Date.now()
-  	}).save( function ( err, need, count ){
-    	if( err ) return next( err );
-    	console.log('insert need successed');
-    	res.redirect( 'home' );
-  	});
+exports.create = function(req, res) {
+	// console.log(req.body.name);
+	new Need({
+		name: req.body.name,
+		updated_at: Date.now()
+	}).save(function(err, need, count) {
+		if (err) return next(err);
+		console.log('insert need successed');
+		res.redirect('home');
+	});
 
 };
 
-exports.more = function (req, res) {
- 	console.log('in more model ',req.params.id);
- 	Need.
- 		find({_id: req.params.id}).
- 		exec( function (err, item){
-	    	if(err){
-	      		console.error(err);
-	    	};
-	    	res.json(item);
-	    });       		 	
+exports.more = function(req, res) {
+	console.log('in more model ', req.params.id);
+	Need.
+	find({
+		_id: req.params.id
+	}).
+	exec(function(err, item) {
+		if (err) {
+			console.error(err);
+		};
+		res.json(item);
+	});
 }
 
-exports.upload = function (req, res) {
-	res.render('upload',{title: 'Love Spreading'});
+exports.upload = function(req, res) {
+	res.render('upload', {
+		title: 'Love Spreading'
+	});
 }
 
-exports.login = function(req, res){
+exports.login = function(req, res) {
 	console.log("login function");
 	var account = req.account;
 	var password = req.password;
-	console.log(account+ " " + password);
+	console.log(account + " " + password);
 }
- // vote.save(function(err, newVote){
-    
- //    var p = [];
- //    var voting = [];
- //    var sum = 0;
- //    Vote.aggregate(
- //      { $group: { _id: "$vote", num: {$sum : 1}}}
- //      , function (err, result) {
- //        if (err) console.log(err);
- //        console.log(result); // [ { maxBalance: 98000 } ]
- //        promise.all(result).then(function(values){
- //          for(var i = 0, len= values.length; i < len; i++){
- //            voting[values[i]._id] = values[i].num;
- //            sum += values[i].num;
- //          }
- //          for(var j = 0; j < 7; j++){
- //            p[j] = voting[j]/sum * 100;
- //            console.log("p: " + p[j]);
- //          }
- //          res.render('result', {
- //            votes: [p[0], p[1], p[2], p[3], p[4], p[5], p[6]]// Percentages
- //          });
- //        });
- //    });
- //  });
+// vote.save(function(err, newVote){
+
+//    var p = [];
+//    var voting = [];
+//    var sum = 0;
+//    Vote.aggregate(
+//      { $group: { _id: "$vote", num: {$sum : 1}}}
+//      , function (err, result) {
+//        if (err) console.log(err);
+//        console.log(result); // [ { maxBalance: 98000 } ]
+//        promise.all(result).then(function(values){
+//          for(var i = 0, len= values.length; i < len; i++){
+//            voting[values[i]._id] = values[i].num;
+//            sum += values[i].num;
+//          }
+//          for(var j = 0; j < 7; j++){
+//            p[j] = voting[j]/sum * 100;
+//            console.log("p: " + p[j]);
+//          }
+//          res.render('result', {
+//            votes: [p[0], p[1], p[2], p[3], p[4], p[5], p[6]]// Percentages
+//          });
+//        });
+//    });
+//  });
 
 
 
@@ -214,3 +228,9 @@ exports.login = function(req, res){
 
 //   next();
 // };
+
+exports.status = function(req, res) {
+	res.render("status", {
+		session: JSON.stringify(req.session)
+	});
+}
