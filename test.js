e2f58ec7d6dc07049d6be6c217dffd0c10a2038b
shelley57123 <shelleysun1992@gmail.com>
2014-06-01 03:12:57 +0800

    test mail

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var gmailer = require("./lib/gmailer.js");
 var mailOptions = {
 	from: "lovespreading2014@gmail.com", // sender address
 	to: "shelleysun1992@gmail.com", // list of receivers
-	subject: "Register Success!", // Subject line
-	html: "Dcard" // html body
+	subject: "Transaction Success!", // Subject line
+	html: "Your items on Love Spreading has been bought by someone else!" // html body
 }
 
 gmailer.sendGmail(mailOptions);
