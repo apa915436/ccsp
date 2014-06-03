8230adec855824289e6ff34de08148d3e7788cb4
Andy Chang <andyaoao@gmail.com>
2014-06-03 22:17:42 +0800

    add flatUI

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,7 @@
 (function(){
 
-var item = '<div class="fix_block block"><div class="thumbnail"><span class="holder"><img src="../img/mouse.png" alt="" width="150px" height="150px"></span><div class="caption"><div class="row-fliud"><div class="col-sm-8"><h3></h3><p></p></div><br><br><a class="btn btn-info pull-right">More</a></div></div></div></div>',
-    donate = '<div class="fix_block block"><div class="thumbnail"><span class="holder"><img src="../img/chair.jpg" alt="" width="150px" height="150px"></span><div class="caption"><div class="row-fliud"><div class="col-sm-8"><h3></h3><p></p></div><br><br><a class="btn btn-info pull-right">More</a></div></div></div></div>';
+var item = '<div class="fix_block thumbnail"><img src="../img/mouse.png" alt="" width="140px" height="140px"><div class="caption"><div class="row-fliud"><div class="col-sm-8"><h6></h6><p></p></div><br><a class="btn btn-info down pull-right">More</a></div></div>',
+    donate = '<div class="col-sm-4 thumbnail"><img src="../img/chair.jpg" alt="" width="140px" height="140px"><div class="caption"><div class="row-fliud"><div class="col-sm-8"><h6></h6><p></p></div><br><a class="btn btn-info down pull-right">More</a></div></div></div>&nbsp&nbsp';
 
 var addButton       = $('#add'),
     charity_btn     = $('#charity'),   
@@ -50,7 +50,7 @@ function load(cat, bind){
             var a = $(item).appendTo(flag);
             var href = '/item/'+bool_charity+'/'+items[i].supply_id;
             console.log('item id',items[i].supply_id);
-            a.find('h3').text('商品名稱: '+items[i].item_name);
+            a.find('h6').text('商品名稱: '+items[i].item_name);
             a.find('p').text('Credit: '+items[i].credit);
             a.find('a').attr('data-id', items[i].supply_id);
             a.find('a').attr('href', href);
@@ -73,7 +73,7 @@ function need(cat,bind){
             var a = $(donate).appendTo(flag);
             var href = '/item/'+bool_charity+'/'+items[i].need_id;
             // console.log('item id',items[i]._id);
-            a.find('h3').text('商品名稱: '+items[i].item_name);
+            a.find('h6').text('商品名稱: '+items[i].item_name);
             a.find('p').text('需求數量: '+items[i].amount);
             a.find('a').attr('data-id', items[i].need_id);
             a.find('a').attr('href', href);
