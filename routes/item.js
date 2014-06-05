1eb8b60941a2194d9215be7a0995c9929cd204d3
apa915436 <apa915436@gmail.com>
2014-06-05 11:53:41 +0800

    email

--- routes/item.js
+++ routes/item.js
@@ -133,7 +133,7 @@ exports.deal = function(req, res){
 
 			var mailOptions = {
 				from: "lovespreading2014@gmail.com", // sender address
-				to: users[0].email, // list of receivers
+				to: supplier_mail, // list of receivers
 				subject: "Transaction Success on Love Spreading!", // Subject line
 				html: "Congratulation!!<br>"+
 				"Your items on Love Spreading has been bought by someone else!<br><br>"+
